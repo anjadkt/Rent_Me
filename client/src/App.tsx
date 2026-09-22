19181bbc726a3e78a690abafc9c041054b63f2c0
anjadkt <anjad076@gmail.com>
2026-09-22 11:46:34 +0530

    feat: created auth,rental,vehicles page layouts and implemented using react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import Auth from "./pages/Auth";
+import Rentals from "./pages/Rental";
+import Vehicles from "./pages/Vehicles";
 
 function App() {
-
   return (
-    <></>
-  )
+      <Routes>
+        <Route path="/" element={<Vehicles />} />
+        <Route path="/rentals" element={<Rentals />} />
+        <Route path="/auth" element={<Auth />} />
+      </Routes>
+  );
 }
 
-export default App
+export default App;
